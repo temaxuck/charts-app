8b9abcbbddbf9666cb76e62773dba1f734f42421
temaxuck <gorropand@gmail.com>
2025-03-16 19:28:02 +0300

    Refactor errors & update interface

--- client/src/lib/exceptions.ts
+++ client/src/lib/exceptions.ts
@@ -1,13 +1,31 @@
-export class AuthError extends Error {
+export class ErrorBase extends Error {
   private _details: string | null = null;
 
   constructor(msg: string, details: string | null) {
     super(msg);
     this._details = details;
-    Object.setPrototypeOf(this, AuthError.prototype);
+    Object.setPrototypeOf(this, ErrorBase.prototype);
   }
 
   get details(): string | null {
     return this._details;
   }
+
 }
+
+export class AuthError extends ErrorBase {
+  constructor(details: string | null) {
+    super("Authentication Error", details);
+    Object.setPrototypeOf(this, AuthError.prototype);
+  }
+}
+
+
+export class ParseError extends ErrorBase {
+  constructor(details: string | null) {
+    super("Parse Error", details);
+    Object.setPrototypeOf(this, AuthError.prototype);
+  }
+}
+
+
