88d3f7cc310e688f28da95a85131f5548bb3a1c3
temaxuck <gorropand@gmail.com>
2025-03-16 19:26:34 +0300

    Update config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
-    plugins:[
+    plugins: [
         tsconfigPaths()
     ]
 });
