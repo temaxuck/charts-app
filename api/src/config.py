09a829c17faf370b17674124ced943d16593ab70
temaxuck <gorropand@gmail.com>
2025-03-16 09:12:46 +0300

    Prepare project structure

---
+++ api/src/config.py
@@ -0,0 +1,9 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+from secrets import token_hex
+from typing import Set
+
+
+class Config(BaseSettings):
+    SECRET_KEY: SecretStr = token_hex(32)
+    ALLOWED_ORIGINS: Set[str] = set(["http://localhost:3000"])
