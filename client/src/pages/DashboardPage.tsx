f4ba03ba4df386e962ef2398a8d2a2ad704e7a93
temaxuck <gorropand@gmail.com>
2025-03-16 18:42:10 +0300

    Add header to dashboard

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -2,13 +2,17 @@ import { Container, Typography } from "@mui/material";
 import * as React from "react";
 
 import Chart from "../components/Chart";
+import Header from "../components/Header";
+import LogoutButton from "../components/LogoutButton";
 import withAuth from "../components/WithAuth";
 
 function DashboardPage() {
-
   return (
     <Container component="main">
-      <Typography variant="h5">Dashboard</Typography>
+      <Header>
+        <Typography variant="h5" sx={{ mb: 1 }}>Dashboard</Typography>
+        <LogoutButton />
+      </Header>
       <Chart currentValue={90} maxValue={100} />
     </Container>
   );
