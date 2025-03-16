27d28859761421d371173d20e6eea17d6832944d
temaxuck <gorropand@gmail.com>
2025-03-16 19:27:36 +0300

    Move get api url to external function

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,3 @@
+export function getApiURL() {
+    return import.meta.env.VITE_API_URL;
+}
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -7,13 +7,14 @@ import LoginForm from "../components/LoginForm";
 import { loginRequest } from "../lib/api";
 import { getAccessToken, setAccessToken } from "../lib/auth";
 import { AuthError } from "../lib/exceptions";
+import { getApiURL } from "../lib/utils";
 
 export default function LoginPage() {
   const [error, setError] = React.useState<string | null>(null);
   const navigate = useNavigate();
 
   const handleLogin = async (username: string, password: string) => {
-    const apiUrl = `${import.meta.env.VITE_API_URL}/login`;
+    const apiUrl = `${getApiURL()}/login`;
     try {
       const accessToken = await loginRequest(apiUrl, username, password);
       setAccessToken(accessToken);
@@ -40,7 +41,7 @@ export default function LoginPage() {
       <Header>
         <Typography variant="h5" sx={{ mb: 1 }}>Sign in</Typography>
       </Header>
-      {error && <Typography color="error">{error}</Typography>} {/* Display the error message */}
+      {error && <Typography color="error">{error}</Typography>}
       <LoginForm onLogin={handleLogin} />
     </Container>
   );
