3ad6182a3858388d6a272b74b917e400175762eb
temaxuck <gorropand@gmail.com>
2025-03-16 19:29:20 +0300

    Implement Dashboard page

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,19 +1,45 @@
 import { Container, Typography } from "@mui/material";
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
 import Chart from "../components/Chart";
 import Header from "../components/Header";
+import Loader from "../components/Loader";
 import LogoutButton from "../components/LogoutButton";
 import withAuth from "../components/WithAuth";
+import { getChartData } from "../lib/api";
+import { removeAccessToken } from "../lib/auth";
+import { AuthError } from "../lib/exceptions";
+import { ChartData } from "../lib/models";
+import { getApiURL } from "../lib/utils";
 
 function DashboardPage() {
+  const [chartData, setChartData] = React.useState<ChartData | null>(null);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    getChartData(`${getApiURL()}/chart-data`)
+      .then((chartData) => setChartData(chartData))
+      .catch((err) => {
+        if (err instanceof AuthError) {
+          removeAccessToken();
+          navigate("/login");
+        } else {
+          console.error(err);
+        }
+      });
+  }, []);
   return (
     <Container component="main">
       <Header>
         <Typography variant="h5" sx={{ mb: 1 }}>Dashboard</Typography>
         <LogoutButton />
       </Header>
-      <Chart currentValue={90} maxValue={100} />
+      {
+        chartData
+          ? <Chart currentValue={chartData.value} maxValue={chartData.max} />
+          : <Loader />
+      }
     </Container>
   );
 }
