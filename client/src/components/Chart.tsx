81aea1db4568ce199e6a28da31a2efca0a16ba31
temaxuck <gorropand@gmail.com>
2025-03-16 17:04:31 +0300

    Implement chart

--- client/src/components/Chart.tsx
+++ client/src/components/Chart.tsx
@@ -1,15 +1,54 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Card, CircularProgress, Divider, Typography } from "@mui/material";
 import * as React from "react";
 
 export interface Props {
-  percentage: number;
+  currentValue: number;
+  maxValue: number;
 }
 
-export default function Chart({ percentage }: Props) {
+export default function Chart({ currentValue, maxValue }: Props) {
+  const percentage = currentValue / maxValue * 100;
+
   return (
-    <Box>
-      <Typography variant="h4">Chart</Typography>
-      <Typography variant="h3">{percentage}</Typography>
+    <Card variant="outlined" sx={{ maxWidth: 360 }}>
+      <Box sx={{ p: 2 }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Chart data
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {Math.round(currentValue)} / {Math.round(maxValue)}
+        </Typography>
+
+      </Box>
+      <Divider />
+      <Box sx={{ p: 2, display: "flex", justifyContent: "center" }}>
+        <DiagramChart percentage={percentage} />
+      </Box>
+    </Card>
+  );
+}
+
+
+function DiagramChart({ percentage }: { percentage: number }) {
+  return (
+    <Box sx={{ position: "relative", display: "inline-flex" }}>
+      <CircularProgress variant="determinate" value={percentage} size={100} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h5">
+          {`${Math.round(percentage)}%`}
+        </Typography>
+      </Box>
     </Box>
   );
 }
