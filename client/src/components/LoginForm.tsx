59eccfad8136be4055f5ba78887c25e687eb710b
temaxuck <gorropand@gmail.com>
2025-03-16 16:33:14 +0300

    Implement LoginForm

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import * as React from "react";
 
 export interface Props {
@@ -6,9 +6,36 @@ export interface Props {
 }
 
 export default function LoginForm({ onLogin }: Props) {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onLogin(username, password);
+  };
+
   return (
-    <Box>
-      <Typography variant="h4">Login form</Typography>
+    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        label="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Sign In
+      </Button>
     </Box>
   );
 }
