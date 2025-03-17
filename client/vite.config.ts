c89ba3718144e22d16ba0cac7ecc0b9a35cca8d1
temaxuck <gorropand@gmail.com>
2025-03-17 09:37:37 +0300

    Add docker support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+
 export default defineConfig({
-    plugins: [
-        tsconfigPaths()
-    ]
-});
+  plugins: [tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+})
